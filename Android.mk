b999aecd44082f7bd0d7693953f40566b9579e64
Doug Kwan <dougkwan@google.com>
2011-07-16 17:59:33 -0700

    Add private copies of sincos*().  We need these to build libstlport.so with gcc 4.6 correctly.
    Change-Id: If080ec0540a65327147c7bb4c9bb954ccb20bd95

--- Android.mk
+++ Android.mk
@@ -42,6 +42,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(libstlport_src_files)
 
+# This is needed to work around the problem that libm.so in NDK does not
+# have the sincos*() functions.
+
+LOCAL_SRC_FILES += src/sincos.c
+
 LOCAL_MODULE := libstlport
 
 LOCAL_CFLAGS := $(libstlport_cflags)
