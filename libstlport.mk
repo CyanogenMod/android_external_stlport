4cb2ade36a3f7483a9edc3b7788fb8b5ab749ece
Dan Albert <danalbert@google.com>
2014-09-15 12:54:26 -0700

    Use the build system's STL selection.
    This file is only kept around for compatibility so the change won't have
    to go in as one mega-CL.
    
    Change-Id: If712ba0ae7908d8147a69e29da5c453a183d6540

--- libstlport.mk
+++ libstlport.mk
@@ -1,12 +1,3 @@
-# To use stlport, "include external/stlport/libstlport.mk" in your target.
-
-# We put the STL libraries in front of any user libraries, but we need to
-# keep the RTTI stuff in abi/cpp/include in front of our STL headers.
-LOCAL_C_INCLUDES := \
-	$(filter abi/cpp/include,$(LOCAL_C_INCLUDES)) \
-	external/stlport/stlport \
-	bionic \
-        bionic/libstdc++/include \
-	$(filter-out abi/cpp/include,$(LOCAL_C_INCLUDES))
-
-LOCAL_SHARED_LIBRARIES += libstlport
+# New projects shouldn't use this file. To use libc++, add the following line to
+# your makefile.
+LOCAL_CXX_STL := stlport
